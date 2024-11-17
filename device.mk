b84e6b818d4477baaa7a485fb9ad568c133d2ad9
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-11-18 00:26:46 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,6 +6,22 @@
 #
 
 LOCAL_PATH := device/nothing/Spacewar
+
+# Configure Virtual A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Enable developer GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# OTA Assert
+TARGET_OTA_ASSERT_DEVICE := Spacewar
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -33,3 +49,34 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Display Size & Density
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_DENSITY := 420
+TARGET_SCREEN_WIDTH := 1080
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_BUILD_SUPER_PARTITION  := false
+
+# TWRP notch offsets
+TW_STATUS_ICONS_ALIGN := center
+TW_Y_OFFSET := 99
+TW_H_OFFSET := -99
+
+# TWRP - Crypto
+TW_INCLUDE_CRYPTO               := true
+TW_INCLUDE_CRYPTO_FBE           := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_FORCE_KEYMASTER_VER          := true
+BOARD_USES_QCOM_FBE_DECRYPTION  := true
+PLATFORM_VERSION                := 99.87.36
+PLATFORM_VERSION_LAST_STABLE    := $(PLATFORM_VERSION)
+
+PLATFORM_SECURITY_PATCH := 2127-12-31
+VENDOR_SECURITY_PATCH   := $(PLATFORM_SECURITY_PATCH)
+BOOT_SECURITY_PATCH     := $(PLATFORM_SECURITY_PATCH)
+
+# API
+PRODUCT_SHIPPING_API_LEVEL  := 30
+PRODUCT_TARGET_VNDK_VERSION := 30
